68e1731850676498bb04960a74ff6a06186fd8f4
unknown <mengniyozovogabek02@gmail.com>
2022-02-23 20:25:00 +0500

    AuthMiddlewares

---
+++ src/middlewares/AuthMiddleware.js
@@ -0,0 +1,17 @@
+const { checkToken } = require("../modules/jwt")
+
+module.exports = async function (req, res, next) {
+    let token = req?.cookies?.token || req.headers["authorization"] 
+    // req.cookies bo'lmasa undefined ichidan tokenni qidiradi va error beradi ? qo'ysak cookies bo'lmasa req?.cookies?.token o'zini undefined oladi
+
+    token = checkToken(token)
+
+    if(!token) {
+        res.redirect('/users/login')
+        return
+    }
+
+    req.user = token
+
+    next()
+}
